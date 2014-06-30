9f56a0841903cb03e92ce6e863edd72387b440b7
zxccv <asamoyloff@gmail.com>
2014-06-30 23:45:43 +0400

    Initial commit

---
+++ PingTray/PingTray.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class PingTrayForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerPing = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pingAdres = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timerPing
+            // 
+            this.timerPing.Enabled = true;
+            this.timerPing.Interval = 500;
+            this.timerPing.Tick += new System.EventHandler(this.timerPing_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Адрес для пинга:";
+            // 
+            // pingAdres
+            // 
+            this.pingAdres.Location = new System.Drawing.Point(114, 24);
+            this.pingAdres.Name = "pingAdres";
+            this.pingAdres.Size = new System.Drawing.Size(394, 20);
+            this.pingAdres.TabIndex = 1;
+            // 
+            // PingTrayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 63);
+            this.Controls.Add(this.pingAdres);
+            this.Controls.Add(this.label1);
+            this.Name = "PingTrayForm";
+            this.ShowInTaskbar = false;
+            this.Text = "Ping Tray";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerPing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pingAdres;
+    }
+}
+
---
+++ PingTray/PingTray.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class PingTrayForm : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = CharSet.Auto)]
+        extern static bool DestroyIcon(IntPtr handle);
+
+        private void GetIcon(NotifyIcon notiIcon, string text)
+        {
+            Bitmap bitmap = new Bitmap(32, 32);
+
+            int fontsize = 0;
+            switch (text.Length)
+            {
+                case 0:
+                case 1:
+                case 2:
+                    fontsize = 16;
+                    break;
+                case 3:
+                    fontsize = 15;
+                    break;
+                default:
+                    fontsize = 16;
+                    break;
+            }
+            System.Drawing.Font drawFont = new System.Drawing.Font("Calibri", fontsize, FontStyle.Bold);
+            System.Drawing.SolidBrush drawBrush = new System.Drawing.SolidBrush(System.Drawing.Color.LightGreen);
+
+            System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(bitmap);
+
+            graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixel;
+            graphics.DrawString(text, drawFont, drawBrush, 1, 2);
+
+            Icon createdIcon = Icon.FromHandle(bitmap.GetHicon());
+
+            notiIcon.Icon = createdIcon;
+
+            DestroyIcon(createdIcon.Handle);
+     
+            drawFont.Dispose();
+            drawBrush.Dispose();
+            graphics.Dispose();
+            bitmap.Dispose();
+        }
+
+        NotifyIcon notifyIcon;
+        List<String> pingHistory;
+        String pingHistoryString;
+        long lastReplyTime;
+        Thread doPingThread;
+        
+        public PingTrayForm()
+        {
+            InitializeComponent();
+                        
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            notifyIcon.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon = new NotifyIcon();
+            notifyIcon.MouseClick += new MouseEventHandler(notifyIcon_MouseClick);
+
+            notifyIcon.Visible = true;
+            
+            pingAdres.Text = "ya.ru";
+
+            pingHistory = new List<String>();
+
+            doPingThread = new Thread(DoPing);            
+        }
+
+        void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {                
+                notifyIcon.ShowBalloonTip(10000);
+            };
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                if (this.WindowState == FormWindowState.Minimized)
+                {                    
+                    this.WindowState = FormWindowState.Normal;
+                    this.ShowInTaskbar = true;
+                }
+                else if (this.WindowState == FormWindowState.Normal)
+                {
+                    
+                    this.WindowState = FormWindowState.Minimized;
+                    this.ShowInTaskbar = false;
+                }
+
+            };
+
+            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void DoPing()
+        {
+            if (pingAdres.Text == "")
+                return;
+
+            Ping ping = new Ping();
+
+            long replyTime = 0;
+            String errorString = "";
+
+            try
+            {
+                PingReply reply = ping.Send(pingAdres.Text);
+
+                replyTime = reply.RoundtripTime;
+            }
+            catch (Exception ex)
+            {
+                errorString = ex.InnerException.Message;
+            }
+
+            String ReplyString = Convert.ToString(replyTime);
+
+            lastReplyTime = replyTime;
+
+            if (replyTime == 0)
+                ReplyString += "(" + errorString + ")";
+
+            pingHistory.Add(ReplyString);
+
+            if (pingHistory.Count > 20)
+            {
+                pingHistory.RemoveAt(0);
+            }
+
+            pingHistoryString = "";
+
+            foreach (String pingHistoryElement in pingHistory)
+            {
+                if (pingHistoryString != "")
+                    pingHistoryString += Environment.NewLine;
+
+                pingHistoryString += pingHistoryElement;
+
+            }
+        }        
+
+        private void timerPing_Tick(object sender, EventArgs e)
+        {
+            if (doPingThread.ThreadState != ThreadState.Running)
+            {                
+                doPingThread.Abort();
+                doPingThread = new Thread(DoPing);
+                doPingThread.Start();
+            }
+
+            notifyIcon.BalloonTipText = pingHistoryString;
+            GetIcon(notifyIcon, Convert.ToString(lastReplyTime));        
+        }
+    }
+}
